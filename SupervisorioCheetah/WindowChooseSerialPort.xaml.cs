9c70526e964994e0f3e7434dd0ea5bea981bf62f
Daniel Cezar <danie@DANIEL-PC>
2016-07-14 12:48:39 -0300

    Added Tela de escolha de porta serial

---
+++ SupervisorioCheetah/WindowChooseSerialPort.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SupervisorioCheetah
+{
+    /// <summary>
+    /// Interaction logic for WindowChooseSerialPort.xaml
+    /// </summary>
+    public partial class WindowChooseSerialPort : Window
+    {
+        public string nameConexao { get; set; }
+        public WindowChooseSerialPort()
+        {
+            InitializeComponent();
+            cmbxList.ItemsSource = SerialPort.GetPortNames();
+            cmbxList.SelectedIndex = 0;
+        }
+
+        private void btnRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            cmbxList.ItemsSource = SerialPort.GetPortNames();
+            cmbxList.SelectedIndex = 0;
+        }
+
+        private void btnConect_Click(object sender, RoutedEventArgs e)
+        {
+            nameConexao = (string)cmbxList.SelectedValue;
+        }
+    }
+}
